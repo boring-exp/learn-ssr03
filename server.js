a658c0e250a228f9d7fc7e055cbb5f3f6a203cfa
JinweiOS <jinwei.peng@beingthink.com>
2023-11-19 11:26:22 +0800

    feat: 服务端渲染，未激活

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 import Koa from 'koa'
 import serve from 'koa-static'
 import path from 'node:path'
+import router from 'koa-router'
+import {
+  renderToString,
+  app
+} from './src/client.js'
 
-const app = new Koa();
+const rt = new router()
+rt.get('/', async (ctx) => {
+  const ssrHtml = await renderToString(app)
+  ctx.body = `
+  <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Vue SSR Example</title>
+      </head>
+      <body>
+        <div id="app">${ssrHtml}</div>
+      </body>
+    </html>
+  `
+})
 
-// 静态文件中间件
-console.log(path.join(process.cwd(), '/static'))
-app.use(serve(path.join(process.cwd(), './static')));
 
-app.listen(3000, () => {
+const server = new Koa();
+server.use(rt.routes()).use(rt.allowedMethods())
+server.listen(3000, () => {
   console.log('服务器已启动，监听端口 3000');
 });
